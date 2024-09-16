960ad60f264a6a9d6595f868e36ed7c043d67d50
luhh_fvl <168596833+luanafvl@users.noreply.github.com>
2024-09-16 17:58:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+const vermelho = document.getElementById("vermelho");
+const amarelo = document.getElementById("amarelo");
+const verde = document.getElementById("verde");
+
+
+function setRed(){
+    vermelho.style.backgroundColor = "red";
+    amarelo.style.backgroundColor = "black";
+    verde.style.backgroundColor = "black";
+
+}
+
+function setYellow(){
+    vermelho.style.backgroundColor = "black";
+    amarelo.style.backgroundColor = "yellow";
+    verde.style.backgroundColor = "black";
+}
+
+function setGreen(){
+    vermelho.style.backgroundColor = "black";
+    amarelo.style.backgroundColor = "black";
+    verde.style.backgroundColor = "green";
+}
+
+function start(){
+    setTimeout(() => {
+        setRed()
+    }, 0)
+    setTimeout(() => {
+        setYellow
+    }, 2000);
+    setTimeout(() => {
+        setGreen 
+    }, 4000)
+}
+    
+start();
+
+setInterval(() => {
+    start();
+}, interval);
 
